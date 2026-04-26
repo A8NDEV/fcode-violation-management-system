fe0dc9ac70c057d4c4d04d77346d894b2f430c02
Nguyễn Văn Duy Nhân <129759366+NHANGIOI@users.noreply.github.com>
2026-04-26 23:54:58 +0800

    refactor: move struct definitions from utils to structure

---
+++ include/structure.h
@@ -0,0 +1,23 @@
+#ifndef STRUCTRE_H
+#define STRUCTRE_H
+
+#include <time.h>
+#define LONG_SIZE 50
+#define SHORT_SIZE 15
+typedef struct Member{
+    char fullName[LONG_SIZE],email[LONG_SIZE],phone[SHORT_SIZE],studentId[SHORT_SIZE];
+    int team,role,violationCount,consecutiveAbsences;
+    double totalFine;
+}Member;
+typedef struct Account{
+    char studentId[SHORT_SIZE],password[LONG_SIZE];
+    int role,isLooked,failCount;
+}Account;
+typedef struct Violation{
+    char studentId[SHORT_SIZE],note[LONG_SIZE];
+    int reason,isPaid,penalty;
+    time_t violationTime;
+    double fine;
+}Violation;
+
+#endif
