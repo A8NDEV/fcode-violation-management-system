195f32aa3644fbb93a704288faf81ce6b723587c
KhanhTuong <khanhtuong2302@gmail.com>
2026-04-27 15:54:24 +0700

    fix(auth): connect to main.c

--- include/auth.c
+++ include/auth.c
@@ -1,37 +1,4 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h> 
-#define MAX_ID_LEN 9
-#define MAX_PASS_LEN 50
-#define MAX_ACCOUNT 1000
-//--------------------------------------------------------
-//--------------------------------------------------------
-struct account {
-    char studentId[MAX_ID_LEN];
-    char password[MAX_PASS_LEN];
-    int role;  
-    int isLocked; 
-    int failCount; 
-}; 
-typedef struct account account;
-
-enum menuState { 
-    startState,
-    loginState,
-    logoutState,
-    changePasswordState,
-    exitState
-};
-typedef enum menuState Menu;
-//--------------------------------------------------------
-//--------------------------------------------------------
-    // khai báo các hàm sẽ dùngg 3==D
-void start(account *accountList, int *quantity);
-void login(account *accountList, int *quantity, int *isLogin, Menu *currentState, account *session);
-void setting(int role, Menu *currentState);
-void menu(account *accountList, int *quantity, int *isLogin, account *session);
-void changePassword(account *accountList,int role, int *quantity);
-//--------------------------------------------------------
+#include "auth.h"
 //--------------------------------------------------------
 //FLOW9 CỦA CHƯƠNG TRÌNH
 void menu(account *accountList,int *quantity,int *isLogin,account *session) {
@@ -272,14 +239,3 @@ void changePassword(account *accountList,int role, int *quantity) {
     }
 }
 
-int main() {
-//--------------------------------------------------------
-    account accountList[MAX_ACCOUNT];
-    account session[1];
-    int quantity = 0;
-    int isLogin = 0;
-//--------------------------------------------------------
-    menu(accountList,&quantity,&isLogin,session);
-//--------------------------------------------------------
-    return 0;
-}
