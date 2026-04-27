e190d9f916ddca1934ed5af7e213b9b67c91da2b
KhanhTuong <khanhtuong2302@gmail.com>
2026-04-27 15:57:14 +0700

    refactor main.c

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "auth.h"
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	return 0;
+int main() {
+//--------------------------------------------------------
+    account accountList[MAX_ACCOUNT];
+    account session[1];
+    int quantity = 0;
+    int isLogin = 0;
+//--------------------------------------------------------
+    menu(accountList,&quantity,&isLogin,session);
+//--------------------------------------------------------
+    return 0;
 }
