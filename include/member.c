bff9890b8a1de5a9a5f7e42b0c25238c8970f3c1
Nguyễn Văn Duy Nhân <129759366+NHANGIOI@users.noreply.github.com>
2026-05-01 13:20:35 +0700

    refactor : create_member function to handle account creation and improve file handling

--- include/member.c
+++ include/member.c
@@ -3,46 +3,24 @@
 #include "types.h"
 #include "utils.h"
 #include "validate.h"
+#include "fileio.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
 
-static void create_member(int member_size,Member member_list[],Member new_member){
+static int create_member(int *member_size,Member member_list[],Member new_member,Account account_list[],Account new_account){
     //add to member_list array
-    member_list[member_size + 1] = new_member;
-
+    member_list[*member_size + 1] = new_member;
+    account_list[*member_size + 1] = new_account;
     //add new binary record at end file
-    FILE *fout;
-    fout = fopen("data/members.dat","ab");
-    if(fout == NULL){
-        printf("khong mo duoc file member.dat\n");
-        return;
-    }
-    //add new record to member.dat
-    fwrite(&new_member,sizeof(Member),1,fout);
-    fclose(fout);
-}
-static void create_account(int member_size,Account account_list[],Account new_account){
-    //add to member_list array
-    account_list[member_size + 1] = new_account;
-
-    //add new binary record at end file
-    FILE *fout;
-    fout = fopen("data/accounts.dat","ab");
-    if(fout == NULL){
-        printf("khong mo duoc file account.dat\n");
-        return;
-    }
-    //add new record to account.dat
-    fwrite(&new_account,sizeof(Account),1,fout);
-    fclose(fout);
+    if(Append_members_dat(new_member) == 0 || Append_accounts_dat(new_account) == 0)    return 0;
+    ++(*member_size);
+    return 1;
 }
 static void Menu_create(int *member_size,Member member_list[],Account account_list[]){
     Member new_member = Init_Member();
     Input_fullname(new_member.fullName);
-    Input_email(new_member.email);
-    Input_phone(new_member.phone);
     Input_studentId(new_member.studentId);
     if(Find_studentId(*member_size,member_list,new_member.studentId) != -1){
         printf("Da co member voi MSSV la %s.\nTao tai khoan that bai\n",new_member.studentId);
@@ -50,6 +28,8 @@ static void Menu_create(int *member_size,Member member_list[],Account account_li
         system("cls");
         return;
     }
+    Input_email(new_member.email);
+    Input_phone(new_member.phone);
     Input_team(&new_member.team);
     Input_role(&new_member.role);
 
@@ -57,12 +37,11 @@ static void Menu_create(int *member_size,Member member_list[],Account account_li
     strcpy(new_account.studentId,new_member.studentId);
     Input_password(new_account.password);
     new_account.role = new_member.role;
-    
-    create_account(*member_size,account_list,new_account);
-    create_member(*member_size,member_list,new_member);
-    ++(*member_size);
-    printf("Create new member complete!");
-    Sleep(3000);
+
+    if(create_member(member_size,member_list,new_member,account_list,new_account) == 0){
+        Announcement_error_acction();
+    }
+    else    Announcement_complete_action();
 }
 
 static void remove_member_account(int *member_size,Member member_list[],Account account_list[],int idx){
@@ -82,19 +61,27 @@ static void remove_violation_index(int *violation_size,Violation violation_list[
 }
 static void Menu_remove(int *member_size,Member member_list[],Account account_list[],int *violation_size,Violation violation_list[]){
     char studentId[SHORT_SIZE];
-    int tmp = 0,idx = -1; 
-    while(tmp == 0 || idx == -1){
-        printf("======================================\n");
+    Input_studentId(studentId);
+    int idx = Find_studentId(*member_size,member_list,studentId);
+    while(idx == -1){
+        printf("ko ton tai member voi MSSV %s\n",studentId);
+        printf("Action Fail!\n");
+        int user_choose = -1;
+        printf("[0] | back to memnu\n");
+        printf("[1] | try again\n");
+        Input_user_choose(&user_choose);
+        if(user_choose == 0){
+            system("cls");
+            return;
+        }
         Input_studentId(studentId);
-        if(tmp != 0)    idx = Find_studentId(*member_size,member_list,studentId);
-        if(tmp == 0) Announcement_wrong_format();
-        else if(idx == -1)   Announcement_uaivailable_member();
-        else    break;
+        idx = Find_studentId(*member_size,member_list,studentId);
     }
     remove_member_account(member_size,member_list,account_list,idx);
     for(int i = 0;i < *violation_size;++i)   if(strcmp(studentId,violation_list[i].studentId)){
         remove_violation_index(violation_size,violation_list,i);
     }
+    Announcement_complete_action();
 }
 
 static void Menu_update(){
@@ -108,15 +95,25 @@ void main_menu_CRUD(int *member_size,Member member_list[],Account account_list[]
         printf("[2] | Cap nhat thong tin member\n");
         printf("[3] | Xoa member\n");
         printf("[0] | Thoat\n");
-        printf("Lua chon cua ban : ");
         int user_choose = 0;
-        scanf("%d",&user_choose);
-        clear_buffer();
+        Input_user_choose(&user_choose);
+        system("cls");
 
-        if(user_choose == 0)   break;
-        else if(user_choose == 1)   Menu_create(member_size,member_list,account_list);
-        else if(user_choose == 2)   Menu_update();
-        else if(user_choose == 3)   Menu_remove(member_size,member_list,account_list,violation_size,violation_list);
-        else    Announcement_unaivailable_option();
+        switch(user_choose){
+            case 0:
+                return;
+            case 1:
+                Menu_create(member_size,member_list,account_list);
+                break;
+            case 2:
+                Menu_update();
+                break;
+            case 3:
+                Menu_remove(member_size,member_list,account_list,violation_size,violation_list);
+                break;
+            default:
+                Announcement_unaivailable_option();
+                break;
+        }
     }
 }
