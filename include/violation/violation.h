00a31fbec08019706e31c7b37a13ba118a145439
Nguyễn Văn Duy Nhân <129759366+NHANGIOI@users.noreply.github.com>
2026-05-15 18:27:51 +0700

    refactor: remove unused violation header file

--- include/violation/violation.h
+++
@@ -1,8 +0,0 @@
-#ifndef VIOLATION_H
-#define VIOLATION_H
-
-
-void viewOwnViolations(Violation violations[], int totalViolations, const char* loggedInStudentID, int userViolationCount);
-void check_to_Paid(Violation violation[], int total_Violations, const char* logged_In_StudentID);
-void sort_CLB_Violations(Member member[], int size, int isAscending);
-#endif
